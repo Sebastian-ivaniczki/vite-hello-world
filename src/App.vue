ca65abdf93eed99f82c2ee93ba13e9ac2311aef5
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-19 16:31:02 +0100

    exercice done

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+    components: {
+        AppTitle
+    }
+}
+</script>
+
+<template>
+     <app-title></app-title>
+</template>
+<style></style>
